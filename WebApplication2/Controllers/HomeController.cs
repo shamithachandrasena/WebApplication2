ae1c925bb329e560fc416782a34d00544124a30d
Shamitha Chandrasena <shamithachandrasena@gmail.com>
2024-11-22 21:02:51 +0530

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("custom-api")]
+    public class HomeController : Controller
+    {
+        [HttpGet("hi")]
+        public String hi()
+        {
+            return "hi";
+        }
+    }
+}
